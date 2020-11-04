5b07482989846065a7a1fd1da2366711250cba3f
benkeun <bknarfk@gmail.com>
2020-11-04 15:16:51 -0500

    Added ability to toggle between card Image and Barcode

--- app/src/main/java/com/example/cardchain/Adapter.java
+++ app/src/main/java/com/example/cardchain/Adapter.java
@@ -1,20 +1,28 @@
 package com.example.cardchain;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
-import java.util.List;
-
-import MainFragments.FragmentSlide;
 
 public class Adapter extends PagerAdapter {
-
+    private  MultiFormatWriter multiFormatWriter= new MultiFormatWriter();
     private List<Model> models;
     private LayoutInflater layoutInflater;
     private Context context;
@@ -36,12 +44,39 @@ public class Adapter extends PagerAdapter {
 
     @NonNull
     @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+    public Object instantiateItem(@NonNull final ViewGroup container, final int position) {
         layoutInflater = LayoutInflater.from(context);
-        View view = layoutInflater.inflate(R.layout.item, container, false);
-        ImageView imageView = view.findViewById(R.id.card_img);
-        imageView.setImageResource(models.get(position).getImage());
+        final View view = layoutInflater.inflate(R.layout.item, container, false);
+        final ImageButton imageButton = view.findViewById(R.id.card_img);
+        imageButton.setImageResource(models.get(position).getImage());
 
+        imageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (!models.get(position).isShowingCode()) {
+                    models.get(position).toggle();
+
+                    BitMatrix bitMatrix = null;
+                    try {
+                        //TODO Retrieve barcode images and assign a barcode to each model so that this can be models.get(position).barcode
+                        bitMatrix = multiFormatWriter.encode("6046463501982188", BarcodeFormat.valueOf("CODE_128"), 1000, 400);
+                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                        imageButton.setImageBitmap(bitmap);
+                        LayoutParams layout=new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+                        layout.addRule(RelativeLayout.CENTER_IN_PARENT);
+                        imageButton.setLayoutParams(layout);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }else{
+
+                    models.get(position).toggle();
+                    imageButton.setImageResource(models.get(position).getImage());
+                    imageButton.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT));
+                }
+            }
+        });
         container.addView(view, 0);
         return view;
     }
--- app/src/main/java/com/example/cardchain/Model.java
+++ app/src/main/java/com/example/cardchain/Model.java
@@ -2,6 +2,7 @@ package com.example.cardchain;
 
 public class Model {
     private int image;
+    private boolean showingCode=false;
 
     public int getImage() {
         return image;
@@ -10,6 +11,10 @@ public class Model {
     public void setImage(int image) {
         this.image = image;
     }
+
+    public boolean isShowingCode(){ return showingCode;}
+
+    public void toggle(){this.showingCode=!this.showingCode;}
 
     public Model(int image){
         this.image = image;
